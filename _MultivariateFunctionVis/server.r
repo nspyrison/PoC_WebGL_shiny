2c3d1dc14b269cab7ead7058184a0e052a82f02d
Nicholas Spyrison <spyrison@gmail.com>
2020-08-12 21:02:46 +1000

    generalize grid_pts, repair "cube" data, swap dat_star and dat_func order.

--- _MultivariateFunctionVis/server.r
+++ _MultivariateFunctionVis/server.r
@@ -9,72 +9,22 @@ set.seed(20200629)
 options(rgl.useNULL = TRUE) ## Must be executed BEFORE rgl is loaded on headless devices.
 
 ##TODO 
-#- better simulation of two peak bi-variate dist
-#- Average all of the function pts
-#- pairs()/ splom-like display
-#- kerl density vs a_hull
+# surface: a-hull vs quads of grid.
 
 ##### Global initialize -----
 ## Above server scope and not reactive
 ## Parameters:
 {
   ## Some aesthetics setup:
-  rad_pts        <- .02 ## radius of data points spheres
-  alpha_bbox     <- .9  ## alpha opacity for boundingbox and axes lines
   alpha_a_hull   <- .2  ## alpha opacity for triangles of the exterior alpha hull
-  alpha_red_bslice_agg <- 2 * alpha_a_hull ## alpha opacity for red lines indicating aggregating the bslice
-  col_bg         <- "lightgrey" # "grey80" ## color for the back ground & "emission" arg
-  col_bbox       <- "black"     # "grey20" ## color for the bounding box (widget cell)
-  pal_surfaces   <- c("blue", "red", "cyan", "purple") # RColorBrewer::brewer.pal(n = 4, "Paired") 
-  num_bbox_ticks <- 4L
-  d              <- 3L  ## display dimensionality
+  pal            <- RColorBrewer::brewer.pal(n = 4, "Set2")
+  col_pts        <- pal[1]
+  col_surfaces   <- pal[2]
+  num_bbox_ticks <- 1L ## in addition to 0 tick
+  d              <- 3L ## Display dimensionality, number of front dimensions inc function
   # alpha_surfaces <- .6 ## alpha opacity for surfaces and meshes
   # shine          <- 128  ## "Shininess" of some surfaces, in [0, 128] low values (<50) are too reflective
-  
-  load(file = "./data/df_func_surface2.rda") ## Brings df into global environment.
-  
-  ## local spinifex::col_of() to remove dev depandancy
-  col_of <- function (class, pallet_name = "Dark2") {
-    class <- as.factor(class)
-    .l_lvls <- length(levels(class))
-    if (.l_lvls == 0L) 
-      stop("Length of 'class' cannot be zero.")
-    if (.l_lvls > 12L) 
-      stop("'class' has more than the expected max of 12 levels.")
-    pal <- suppressWarnings(RColorBrewer::brewer.pal(.l_lvls, 
-                                                     pallet_name))
-    pal[as.integer(factor(class))]
-  }
 }
-
-app_hist <- function(gg_df, df_lb_ub){
-  gg_df
-  
-  tidyr::pivot_longer(gg_df, cols = 1L:4L, names_to = "variable", values_to = "value")
-  
-  .med <- median(x)
-  .bins <- nclass.FD(x) ## bins on Freedman-Diaconis, func(n, IQR).
-  .den <-  density(x)
-  .y_q1 <- 3 * summary(.den$y)[2L] ## First quartile of the density of the variable
-  .y_range <- diff(range(.den$y))
-  .x_range <- diff(range(x))
-  
-  x <- as.data.frame(x)
-  colnames(x) <- var_nm
-  ggplot(x, aes_string(x = var_nm)) +
-    geom_histogram(aes(y = ..density..), bins = .bins, colour = "black", fill = "grey") +
-    #geom_density(alpha = .3, fill = "red") +
-    geom_vline(aes(xintercept = .med),
-               color = "blue", linetype = "dashed", size = 1L) + 
-    geom_rect(aes(ymin = -.y_q1, ymax = .y_q1, xmin = lb, xmax = ub),
-              fill = "blue", alpha = .01) +
-    facet_grid(rows = n_bd, vars(cyl))
-    theme_void() + 
-    theme(axis.title.x = element_text()) + 
-    labs(x = var_nm) +
-    coord_fixed(ratio =  .2 * .x_range / .y_range) ## Ratio of y/x
-}
-
 
 ####### Shiny server start -----
 server <- shinyServer(function(input, output, session) { ## Session required.
@@ -91,6 +41,10 @@ server <- shinyServer(function(input, output, session) { ## Session required.
   )
   
   ##### functionVis -----
+  grid_pts <- reactive({
+    req(input$grid_pts)
+    input$grid_pts
+  })
   a_hull_alpha <- reactive(input$a_hull_alpha)
   output$a_hull_alpha <- 
     renderText(paste0("Alpha: ", round(a_hull_alpha(), 2L)))
@@ -99,14 +53,19 @@ server <- shinyServer(function(input, output, session) { ## Session required.
   
   ## 1) dat_raw(); rescaled input data -----
   dat_raw <- reactive({
-    if (input$dat == "grid cube") {
-      return(df[, 1L:3L]) ## df from load(file = "./data/df_func_surface2.rda")
+    if (input$dat == "cube") {
+      cube <- geozoo::cube.solid.grid(p = 3L, n = grid_pts())$points %>% 
+        tourr::rescale() %>% 
+        as.data.frame()
+      p <- ncol(cube)
+      colnames(cube) <- paste0("V", 1:p)
+      return(cube)
     }
     if (input$dat == "simulation") {
       ## Remove spaces
       in_a <- gsub(" ", "", input$sim_mns_a)
       in_b <- gsub(" ", "", input$sim_mns_b)
-      ## as.vector, spliting on ","
+      ## as.vector, delimiting on ","
       mns_a <- as.numeric(unlist(strsplit(in_a, ",")))
       mns_b <- as.numeric(unlist(strsplit(in_b, ",")))
       if (length(mns_a) != length(mns_a)) stop("Means of different length.")
@@ -119,114 +78,105 @@ server <- shinyServer(function(input, output, session) { ## Session required.
       ## Simulate
       sim <- sim_pDim_kCl(means = mns, 
                           sigmas = sigs,
-                          n_points = rep(list(500), length(mns)),
+                          n_points = rep(list(1000), length(mns)),
                           method = "eigen",
                           do_shuffle = FALSE)
       ## rescale to [0,1]
       sim_std <- tourr::rescale(sim)
       ## Discretize to grid
-      sim_std_disc <- apply(sim_std, 2L, function(x) round(x, 1)) %>%
-        as.data.frame()
+      rnd_near <- 1/ grid_pts()
+      sim_std_disc <- apply(sim_std, 2L, function(x){ 
+        round(x / rnd_near, 0L) * rnd_near}
+      ) %>% as.data.frame()
       
       return(sim_std_disc)
     }
   })
   dat_fd <- reactive({
     dat <- dat_raw()
-    x_num <- 1L ## Could change to input or hold one out style
-    y_num <- 2L ## Could change to input or hold one out style
-    dat[, c(x_num, y_num)]
+    fd_nums <- c(1L, 2L) ## Could change to input or hold one out style
+    fd_nms <- colnames(dat)[fd_nums]
+    dat[fd_nms]
   })
   dat_bd <- reactive({
     dat <- dat_raw()
-    x_num <- 1L ## Could change to input or hold one out style
-    y_num <- 2L ## Could change to input or hold one out style
-    dat[, -c(x_num, y_num)]
+    fd_nums <- c(1L, 2L) ## Could change to input or hold one out style
+    bd_nums <- setdiff(1:ncol(dat), fd_nums)
+    bd_nms <- colnames(dat)[bd_nums]
+    dat[bd_nms]
   })
   
-  ## 2) dat_func(); apply function -----
+  
+  
+  ## 2) dat_star(); obs in back dimensions -----
+  dat_star <- reactive({
+    dat_bd   <- dat_bd()
+    ncol_bd  <- ncol(dat_bd)
+    if (ncol_bd == 0) return()
+    ## At least 1 bd:
+    req(input$bd_slice_1)
+    dat_raw <- dat_raw()
+    
+    ## Subset to only the rows within all back dimension slices
+    IS_in_all_bd_slices <- TRUE ## Logical vector of rows in ALL back dimenion slices
+    for(i in 1:ncol_bd){
+      dim         <- dat_bd[, i]
+      slice       <- input[[paste0("bd_slice_", i)]]
+      lb          <- min(slice)
+      ub          <- max(slice)
+      IS_in_slice <- data.frame(dim >= lb & dim <= ub) ## Logical vector of rows within this .dim's slice.
+      IS_in_all_bd_slices <- IS_in_all_bd_slices & IS_in_slice
+    }
+    if (sum(IS_in_all_bd_slices) == 0) stop("Currect slices of the back dimensions contain no observations.")
+    ## A df subset (with all bd slice)
+    dat_star <- dat_raw[IS_in_all_bd_slices, ]
+    
+    ## the subset of dat_raw that is in all back dimension slices
+    return(dat_star)
+  })
+  
+  
+  
+  ## 3) dat_func(); apply function -----
   #### df of the the display-x, -y, and function before aggregation
   dat_func <- reactive({
-    dat_raw <- dat_raw()
-    IS_numeric_col <- apply(dat_raw, 2L, function(x) all(is.numeric(x)))
-    dat_num <- dat_raw[, IS_numeric_col]
+    dat_star <- dat_star()
+    
     if (input$func_nm == "kde2d"){
-      ## TODO; NEED TO GO TO MORE GENERALIZED KERNAL DENSITY????
-      # rgl::ellipse3d(cov(z[, 1:3]), level = 0.68,
-      #           centre = apply(z, 2, mean))
-      den2d <- MASS::kde2d(dat_num[,1], dat_num[,2], n = 10L)
+      ## kde2d on only the front dimensions, of the subset obs in the bd slices
+      den2d <- MASS::kde2d(dat_star[, 1L], dat_star[, 2L], n = grid_pts())
       den2d_cross_join <- merge(den2d$x, den2d$y, all = TRUE)
       dat_func <- data.frame(den2d_cross_join, as.vector(den2d$z))
     }
     if (input$func_nm == "dmvnorm"){
-      col_mns  <- colMeans(dat_num)
-      dat_cov <- cov(dat_num)
-      dmvn <- mvtnorm::dmvnorm(dat_num, mean = col_mns, sigma = dat_cov)
-      dat_func <- cbind(dat_num, dmvn)
+      ## dmvnorm on ALL var, subset to the bd slices
+      col_mns <- colMeans(dat_star)
+      covar <- cov(dat_star)
+      print(input$dat)
+      print(col_mns)
+      print(covar)
+      dmvn <- mvtnorm::dmvnorm(dat_star, mean = col_mns, sigma = covar)
+      dat_func <- cbind(dat_fd_star, dmvn)
     }
     ## Rescale is crutial for a hull to work disp aspect ratio.
-    dat_func <- tourr::rescale(dat_func) %>% 
+    dat_func <- tourr::rescale(dat_func) %>%
       as.data.frame()
-    colnames(dat_func) <- c(paste0("V", 1:2), "func")
-    dat_func
-  })
-  
-  ## 3) dat_star(); obs in backdimensions -----
-  dat_star <- reactive({
-    dat_bd   <- dat_bd()
-    ncol_bd  <- ncol(dat_bd)
-    dat_func <- dat_func()
+    colnames(dat_func) <- c(paste0("V", 1L:2L), "func")
     
-    ## Agggregate the function values within bd slices
-    #req(input$bslice_agg)
-    agg_func <- max ## case when doesn't want to return functions; closure not subsettable.
-    # if(       input$bslice_agg == "max")    {max
-    # } else if(input$bslice_agg == "mean")   {mean
-    # } else if(input$bslice_agg == "median") {median
-    # } else if(input$bslice_agg == "min")    {min}
-    if (ncol_bd != 0) {
-      req(input$bd_slice_1)
-      
-      ## Subset to only the rows within all back dimension slices
-      IS_in_all_bd_slices <- TRUE ## Logical vector of rows in ALL back dimenion slices
-      for(i in 1:ncol_bd){
-        dim         <- dat_bd[, i]
-        slice       <- input[[paste0("bd_slice_", i)]]
-        lb          <- min(slice)
-        ub          <- max(slice)
-        IS_in_slice <- data.frame(dim >= lb & dim <= ub) ## Logical vector of rows within this .dim's slice.
-        IS_in_all_bd_slices <- IS_in_all_bd_slices & IS_in_slice
-      }
-      if (sum(IS_in_all_bd_slices) == 0) stop("Currect slices of the back dimensions contain no observations.")
-      ## A df subset (with all bd slice)
-      dat_star <- dat_func[IS_in_all_bd_slices, ]
-    }
-    dat_star <- dplyr::group_by(dat_star, V1, V2) %>%
+    ## Aggregate across the slice of the backdimensions
+    agg_func <- max ## Previously used input$bslice_agg
+    
+    dat_func <- dat_func %>% dplyr::group_by(V1, V2) %>% 
       dplyr::summarise(.groups = "drop",
                        z_agg = agg_func(func),
                        z_min = min(func),
-                       z_max = max(func)) %>%
+                       z_max = max(func)) %>% 
       as.data.frame()
-    dat_star <- dat_star[complete.cases(dat_star), ] ## Sometimes adds a NULL end line?
     
-    ## Return df of aggregated dat_star; rows in all bd slices, columns: V1:V2, z_agg, z_min, z_max
-    dat_star
+    ## df of 3 front dimensions
+    return(dat_func)
   })
   
-  ## Character vector of the hex colors to use based on first factor or character column
-  pt_color <- reactive({
-    dat_star <- dat_star()
-    dat_star <- dat_star[complete.cases(dat_star), ] ## Row-wise complete
-    IS_fct_col <- sapply(dat_star, function(col){
-      is.factor(col) | is.character(col)
-    })
-    
-    ## If not fct|char columns give dummy color
-    if (sum(IS_fct_col) == 0L) return(rep(col_of("NO_FCT|CHAR"), nrow(dat_star))) 
-    fst_fct_col_num <- Position(function(x) x == T, IS_fct_col) ## Number of first column that is fct|char
-    class <- dat_star[, fst_fct_col_num]
-    col_of(class)
-  })
   
   
   ## 4) full_ashape(); superset of triangles -----
@@ -234,73 +184,68 @@ server <- shinyServer(function(input, output, session) { ## Session required.
   #### Contains list elements for each of the shapes: (tetra, triang, edge, vertex, x),
   #### Delaunay triangulations maximizes the smallest angle of the triangles to avoid sliver triangles.
   full_ashape <- reactive({
-    dat_star_3mat <- as.matrix(dat_func())
-    ## Possible alpha values for the a_hull_alpha()
-    a_hull_alpha_seq <- seq(1L, 20L, by = 1L) ## fixed to 1 atm
-    ## ashape3d obj of all alphas and all shapes (tetra, triang, edge, vertex, x)
-    alphashape3d::ashape3d( ## Expects numeric matrix in 3 dimensions.
-      x = dat_star_3mat, alpha = a_hull_alpha_seq, pert = TRUE)
+    if(input$DO_DISP_a_hull_triang == TRUE){
+      dat_star_3mat <- as.matrix(dat_func())
+      ## Possible alpha values for the a_hull_alpha()
+      a_hull_alpha_seq <- seq(1L, 20L, by = 1L)
+      ## ashape3d obj of all alphas and all shapes (tetra, triang, edge, vertex, x)
+      ret <- alphashape3d::ashape3d( ## Expects numeric matrix in 3 dimensions.
+        x = dat_star_3mat, alpha = a_hull_alpha_seq, pert = TRUE)
+      return(ret)
+    }
   })
   
   ## 5) Render scene -----
   scene_functionVis <- reactive({
-    req(dat_star())
-    ##### Init a_hull triangles via alphashape3d::ashape3d()
-    ## Grab only the triangles from the 3D Delaunay triangulation
-    ashape_triang  <- suppressWarnings(full_ashape()$triang) ## Suppresses: "Warning: Duplicate points were removed."
-    ## Column name specifying the current alpha value:
-    alpha_col_nm    <- paste0("fc:", a_hull_alpha())
-    alpha_col_num   <- which(colnames(ashape_triang) == alpha_col_nm)
-    ## rows of the exterior triangles
-    rows_ext_triang <- ashape_triang[, alpha_col_num] == 2L
-    #### Values can be: 0 (not on a_hull) 1 (interitor triang), 2 (regular), 3 (singular)
-    ## Triangles to display; only those on the exterior of the alpha hull.
-    xyz_rows_ext_triang <- t(ashape_triang[rows_ext_triang, 1L:3L])
-    ## df of the exterior triangles of the bd slices, columns: V1, V2, z_agg, z_min, z_max
+    req(dat_func())
     
-    ## Aesthetic init
-    dat_star   <- dat_star()
+    ## Init
+    dat_star   <- dat_func()
     func_nm    <- input$func_nm
     agg_nm     <- "max" #input$bslice_agg 
     bd_col_nms <- colnames(dat_bd())
     fd_col_nms <- c(colnames(dat_fd()), paste0(agg_nm, " ", func_nm))
     labs <- paste0(c("x, ", "y, ", "z, "), fd_col_nms)
-    if(all.equal(dat_star$z_min, dat_star$z_max) == FALSE)
-      labs[3] <- paste0("z, ", fd_col_nms(3L))
-    pt_col <- pt_color()
-    disp_df <- dat_func() ## Full disp cols for setting aspect ratio
-    x_asp <- 1L / diff(range(disp_df[, 1L]))
-    y_asp <- 1L / diff(range(disp_df[, 2L]))
-    z_asp <- 1L / diff(range(disp_df[, 3L]))
+
     
     ##### rgl scene graphics 
     try(rgl.close(), silent = T) ## Shiny doesn't like rgl.clear() or purrr::
-    open3d(FOV = 0L, zoom = .8, windowRect = c(0,0,1200,1200))
+    open3d(FOV = 0L, zoom = .8) ## Zoom is 1/magification
     title3d(xlab = labs[1L], ylab = labs[2L], zlab = labs[3L])
-    aspect3d(x_asp, y_asp, z_asp)
     bbox3d(xlen = num_bbox_ticks, ylen = num_bbox_ticks, zlen = num_bbox_ticks,
-           color = col_bbox , alpha = alpha_bbox, emission = col_bg, lwd = 1L)
-    #par3d(windowRect = c(0,0,1600,1600), zoom = .7)
+           lwd = 1L)
     ## Change h/w in ui, applied to rglWidgetOutput().
-    ## Zoom is 1/magification, applied in open3d(), above.
-    
     
     ### Data point Spheres
-    spheres3d(x = dat_star[, 1], y = dat_star[, 2], z = dat_star[, 3],
-              radius = rad_pts, col = pt_col)
+    spheres3d(x = dat_star[, 1L], y = dat_star[, 2L], z = dat_star[, 3L],
+              radius = .5 / grid_pts(), col = col_pts)
     ### Add red aggregation lines if needed
     if(all(dat_star$z_min == dat_star$z_max) == FALSE) {
-      segments3d(color = "red", alpha = alpha_red_bslice_agg,
-                 rep(dat_star[, 1], each = 2L), 
-                 rep(dat_star[, 2], each = 2L), 
+      segments3d(color = "red", alpha = 2L * alpha_a_hull,
+                 rep(dat_star[, 1L], each = 2L), 
+                 rep(dat_star[, 2L], each = 2L), 
                  c(rbind(dat_star$z_min, dat_star$z_max)))
     }
-    ### a_hull triangs
-    if (input$DO_DISP_a_hull_triang) {
+    ### Add a_hull triangs, if needed
+    if (input$DO_DISP_a_hull_triang == TRUE) {
+      ##### Init a_hull triangles via alphashape3d::ashape3d()
+      ## Grab only the triangles from the 3D Delaunay triangulation
+      ashape_triang  <- suppressWarnings(full_ashape()$triang) ## Suppresses: "Warning: Duplicate points were removed."
+      ## Column name specifying the current alpha value:
+      alpha_col_nm    <- paste0("fc:", a_hull_alpha())
+      alpha_col_num   <- which(colnames(ashape_triang) == alpha_col_nm)
+      ## rows of the exterior triangles
+      rows_ext_triang <- ashape_triang[, alpha_col_num] == 2L
+      #### Values can be: 0 (not on a_hull) 1 (interitor triang), 2 (regular), 3 (singular)
+      ## Triangles to display; only those on the exterior of the alpha hull.
+      xyz_rows_ext_triang <- t(ashape_triang[rows_ext_triang, 1L:3L])
+      ## df of the exterior triangles of the bd slices, columns: V1, V2, z_agg, z_min, z_max
+      
+      ##### Add the triangles
       triangles3d(dat_star[xyz_rows_ext_triang, "V1"],
                   dat_star[xyz_rows_ext_triang, "V2"],
                   dat_star[xyz_rows_ext_triang, "z_agg"],
-                  col = pal_surfaces[1], alpha = alpha_a_hull)
+                  col = col_surfaces[1], alpha = alpha_a_hull)
     }
     
     scene_functionVis <- scene3d()
@@ -317,7 +262,7 @@ server <- shinyServer(function(input, output, session) { ## Session required.
     dat_bd <- dat_bd()
     bd_col_nms <- colnames(dat_bd)
     ## Half of the targeted relative variable volume
-    half_def_rel_size <- .5 * input$tgt_rel_h^(1 / (p - d))
+    half_tgt_slice <- .5 * input$tgt_rel_h^(1L / (p - d))
     
     ## Make slider numeric inputs for the back dimension slice
     ncol_bd <- ncol(dat_bd)
@@ -328,8 +273,8 @@ server <- shinyServer(function(input, output, session) { ## Session required.
       sliderInput(inputId = paste0("bd_slice_", i), 
                   label = paste0(bd_col_nms[i], " slice midpoint"),
                   min = 0L, max = 1L, step = .05,
-                  value = c(med - half_def_rel_size, ## Creates a width slider when you give 2 values.
-                            med + half_def_rel_size))
+                  value = c(med - half_tgt_slice, ## Creates a width slider when you give 2 values.
+                            med + half_tgt_slice))
     })
     
     ## Layout the midpoint and width sliders in a row
@@ -380,9 +325,9 @@ server <- shinyServer(function(input, output, session) { ## Session required.
       facet_wrap(~ var_nm, ncol = 1L) +
       theme_void()
     ## Return
-    fd_hists
+    return(fd_hists)
   })
-  output$fd_histograms <- renderPlot(fd_histograms(), height = 200, width = 200)
+  output$fd_histograms <- renderPlot(fd_histograms())
     
     
   ## Back dimension histograms -----
@@ -443,7 +388,7 @@ server <- shinyServer(function(input, output, session) { ## Session required.
       facet_wrap(~ var_nm, ncol = 1L) +
       theme_void()
     ## Return
-    bd_hists
+    return(bd_hists)
   })
   output$bd_histograms <- renderPlot(bd_histograms(), height = 400, width = 200)
   
--- _MultivariateFunctionVis/ui.r
+++ _MultivariateFunctionVis/ui.r
@@ -9,34 +9,40 @@ require("RColorBrewer")
 require("ggplot2")
 require("lqmm")
 require("mvtnorm")
+require("geozoo")
 require("dplyr")
 require("tidyr")
 
-w <- h <- "800px" ## height and width of the rgl widget in pixels, as applied in UI *Output() function.
-def_rel_h <- .25  ## .25 as per [Laa et al. 2019] Hole or Grain 5.1 #3.
-
+w <- h <- "800px" ## Height and width, as applied in UI *Output() function.
+def_rel_h <- 2L   ## .25 suggested by [Laa et al. 2019] Hole or Grain 5.1 #3.
 
 ##### Start of shiny ui -----
-backDimensionSlices_panel <- tabPanel("Slicing on back-dimenions", fluidPage(
+backDimensionSlices_panel <- tabPanel("Slicing on back dimensions", fluidPage(
   sidebarPanel(width = 3L, fluidRow(
-    selectInput("dat", label = "Data", choices = c("simulation", "cube")),
+    column(width = 6L,
+           selectInput("dat", label = "Data", choices = c("simulation", "cube"))
+    ),
+    column(width = 6L,
+           numericInput("grid_pts", "Number of grid points", 
+                        25L, min = 10L, max = 100L)
+    ),
     selectInput("func_nm", "Function to apply", choices = c("kde2d", "dmvnorm")),
     conditionalPanel(
       "input.dat == 'simulation'",
-      textInput('sim_mns_a', 'Variable means for cluster a (comma delimited)', "0,8,0,0"),
-      textInput('sim_mns_b', 'Variable means for cluster b (comma delimited)', "8,0,0,0"),
+      textInput('sim_mns_a', 'Variable means for cluster a (comma delimited)', "0,5,0,0"),
+      textInput('sim_mns_b', 'Variable means for cluster b (comma delimited)', "5,0,0,0"),
       p("Covariance matrices fixed to identity(p).")
     ),
     p("Back slice is aggregating to the max of all points, with red bar extending to the minimum."),
     # selectInput("bslice_agg", "Back slice aggregation",
     #             choices =  c("max", "mean", "median", "min")),
-    numericInput("tgt_rel_h", "Target fraction of backdimension volume (slice widths adjust to x^(1/p-d))",
-                 value = def_rel_h, min = .05, max = 1L, step = .05),
+    numericInput("tgt_rel_h", HTML("Target fraction of the back dimension volume <br/> (adjusts slice widths to x^(1/p-d))"),
+                 value = def_rel_h, min = .05, max = 2L, step = .05),
     uiOutput("bd_slices_ui"),
     column(width = 6L,
            shinyWidgets::switchInput(inputId = "DO_DISP_a_hull_triang", 
                                      label = "Display alpha hull triangles", 
-                                     value = TRUE)
+                                     value = FALSE)
     ),
     column(width = 6L,
            conditionalPanel(
@@ -53,9 +59,9 @@ backDimensionSlices_panel <- tabPanel("Slicing on back-dimenions", fluidPage(
     fluidRow(
       column(width = 2L, 
              h4("Front dimensions"),
-             plotOutput("fd_histograms"),
+             plotOutput("fd_histograms", width = "200px", height = "150px"), ## w/h set in sever to output assignment
              h4("Back dimensions"),
-             plotOutput("bd_histograms")
+             plotOutput("bd_histograms", width = "200px", height = "450px") ## w/h set in sever to output assignment
       ),
       column(width = 10L, 
              rglwidgetOutput("widget_functionVis", w, h)
